815de8d0d3d1cf876a3ed6161693c44ed721b8e2
quandohong28 <quandohong28@gmail.com>
2023-10-30 13:32:33 +0700

    fix some bug

--- user/result/detail_exam.php
+++ user/result/detail_exam.php
@@ -3,8 +3,8 @@
         <h3 class="fw-bold">Kết quả bài thi</h3>
     </div>
     <div class="container bg-primary-gradient p-lg-5 p-sm-2">
-        <div class="general-inf-user filter px-5 py-3 bg-white shadow rounded">
-            <div class="row">
+        <div class="general-inf-user filter p-5 bg-white shadow rounded mb-5">
+            <div class="row gap-5 justify-content-center align-items-center">
                 <div class="col-md-2">
                     <img src="https://github.com/mdo.png" alt="..." style="width: 150px; height: 150px;" class="rounded-circle">
                 </div>
@@ -12,23 +12,23 @@
                     <div class="col d-flex justify-content-center align-items-center">
                         <div class="col">
                             <div class="col-md-12 pb-1 border-bottom">
-                                <h4 class="text-primary text-uppercase">Đỗ Hồng Quân</h4>
-                                <h6>Bai thi: Toan thong ke</h6>
+                                <h5 class="text-primary text-uppercase">Đỗ Hồng Quân</h5>
+                                <h6>Bài thi: Toan thong ke</h6>
                             </div>
                             <div class="col-md-12 mt-4">
                                 <div class="col d-flex">
                                     <div class="card-body">
-                                        <h5 class="card-title">Diem: 8</h5> 
-                                    </div> 
+                                        <h6 class="card-title">Diem: 8</h6>
+                                    </div>
                                     <div class="card-body">
-                                        <h5 class="card-title">So cau dung: 8</h5> 
-                                    </div> 
+                                        <h6 class="card-title">So cau dung: 8</h6>
+                                    </div>
                                     <div class="card-body">
-                                        <h5 class="card-title">So cau sai: 2</h5> 
-                                    </div> 
+                                        <h6 class="card-title">So cau sai: 2</h6>
+                                    </div>
                                     <div class="card-body">
-                                        <h5 class="card-title">Thoi gian lam bai: 20'</h5> 
-                                    </div>  
+                                        <h6 class="card-title">Thoi gian lam bai: 20'</h6>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -36,10 +36,7 @@
                 </div>
             </div>
         </div>
-        <div class="title">
-            <h4 class="w-100 mb-3 text-center">Chi tiet bai thi</h4>
-        </div>
-        <div class="row row-cols-2"> 
+        <div class="row row-cols-2">
             <?php for ($i = 0; $i < 6; $i++) : ?>
                 <div class="col">
                     <ul class="list-group" style="list-style-type: none;">
@@ -48,20 +45,18 @@
                                 <div class="question px-3 py-2 mb-2" style="background-color: #5c5c5c10; border-radius: .3rem">
                                     <strong>Cau 1: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque debitis est, vel laborum?</strong>
                                 </div>
-                                <ul class="p-0 m-0" style="list-style-type: none; font-size: 18px;">                        
-                                    <li>
-                                        <?php for ($j = 0; $j < 4; $j++) : ?>
-                                            <div class="answer d-flex">
-                                                <img class="ms-1 mt-1 pe-1" style="height: 20px" src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-ios7-circle-outline-256.png" alt="">
-                                                <p class="text m-0">Lorem ipsum dolor sit amet consectetur adipisicing elit.solor blanditiis soluta</p>                                            
-                                            </div>
-                                        <?php endfor; ?>
-                                    </li>
-                                </ul>  
-                            </div>                                  
+                                <ul class="p-0 m-0" style="list-style-type: none; font-size: 18px;">
+                                    <?php for ($j = 0; $j < 4; $j++) : ?>
+                                        <li class="answer d-flex align-items-center gap-3 mb-3">
+                                            <input type="radio" id="answer" name="answer">
+                                            <label class="text m-0 small" for="answer">Lorem ipsum dolor sit amet consectetur adipisicing elit.solor blanditiis soluta</label>
+                                        </li>
+                                    <?php endfor; ?>
+                                </ul>
+                            </div>
                         </li>
                     </ul>
-                </div> 
+                </div>
             <?php endfor; ?>
         </div>
         <ul class="pagination pagination-sm justify-content-end">
@@ -82,4 +77,3 @@
     </div>
 
 </section>
- 
