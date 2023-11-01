8880f87d2a48266d5fa004b40e6ba664b7da3c1e
hieukhocnhe <trantrunghieu422@gmail.com>
2023-11-01 14:30:56 +0700

    fix some bu

--- admin/tables/question/questions.php
+++ admin/tables/question/questions.php
@@ -4,41 +4,36 @@
 
 <a class="btn btn-info mb-2" href="?act=tables&data=add_question">Thêm câu hỏi</a>
 
+<?php for ($i = 0; $i < 10; $i++): ?>
+    <div class="expandable-container">
+        <div id="toggleButton" class="py-2 px-5 mb-3 bg-light text-dark d-flex justify-content-between">
+            <div>
+                Lorem100
+            </div>
 
-<table class="table table-striped">
-    <thead>
-        <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Tên lịch thi</th>
-            <th scope="col">Thời gian bắt đầu</th>
-            <th scope="col">Thời gian kết thúc</th>
-            <th scope="col">Thời gian làm bài</th>
-            <th scope="col">Số lượng đề thi</th>
-            <th scope="col">Số lượng thí sinh</th>
-            <th scope="col">Chức năng</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr class="">
-            <td>R1C1</td>
-            <td>R1C2</td>
-            <td>R1C3</td>
-            <td>R1C3</td>
-            <td>R1C3</td>
-            <td>R1C3</td>
-            <td>R1C3</td>
-            <td class="d-flex gap-1">
-                <a class="btn btn-success btn-sm" href="?act=tables&data=schedule_detail"><i
-                        class="fa-solid fa-circle-info"></i></a>
-                <a class="btn btn-warning btn-sm" href="?act=tables&data=edit_schedule"><i
-                        class="fa-regular fa-pen-to-square"></i></a>
-                <a onclick="return confirm('Bạn có xác nhận xóa ?');" class="btn btn-danger btn-sm"
-                    href="?act=tables&data=del_schedule"><i class="fa fa-trash"></i></a>
+            <a class=" btn btn-warning" href="#" data-toggle="modal" data-target="#editquestionmodal">
+                <i class="fa-regular fa-pen-to-square"></i>
+            </a>
+        </div>
+        <div class="expandable" style="display:none">
+            <!-- Nội dung của thành phần con nằm ở đây -->
+            <div class="text-center">
+                <img src="../assets/img/landing-page-backgound.avif" class="mb-3" alt="" width="500">
+            </div>
+            <ul class="list-group list-group-sm px-5 mb-3">
 
-            </td>
-        </tr>
-    </tbody>
-</table>
+                <li class="list-group-item list-group-item-action text-sm" aria-current="true">
+                    The current link item
+                </li>
+                <li class="list-group-item list-group-item-action text-sm">A second link item</li>
+                <li class="list-group-item list-group-item-action text-sm">A third link item</li>
+                <li class="list-group-item list-group-item-action text-sm">A fourth link item</li>
+
+            </ul>
+        </div>
+    </div>
+<?php endfor ?>
+
 <nav>
     <ul class="pagination pagination-sm justify-content-end">
         <li class="page-item disabled">
@@ -57,5 +52,37 @@
     </ul>
 </nav>
 
+<script>
+    const toggleButtons = document.querySelectorAll("#toggleButton");
+    const expandableContents = document.querySelectorAll(".expandable");
+    toggleButtons.forEach((toggleButton, index) => {
+        toggleButton.addEventListener("click", function () {
+            if (expandableContents[index].style.display === "none" || expandableContents[index].style.display === "") {
+                // Khi ẩn hoặc không hiển thị, thì mở rộng
+                expandableContents[index].style.display = "block";
+            } else {
+                // Khi đã hiển thị, thì thu gọn
+                expandableContents[index].style.display = "none";
+            }
+        });
+    });
+</script>
 
+
+<div class="modal fade" id="editquestionmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+            <div class="modal-footer">
+                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                <a class="btn btn-primary" href="login.html">Logout</a>
+            </div>
+        </div>
+    </div>
 </div>
