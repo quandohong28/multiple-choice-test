06f7ad128eb4290f3703fc37877779cba63e8425
hieukhocnhe <trantrunghieu422@gmail.com>
2023-11-03 21:41:39 +0700

    change some code db/questions.sql

--- assets/db/question.sql
+++ assets/db/question.sql
@@ -55,20 +55,20 @@ VALUES
 
 --Mức trung bình (5) câu
 
-('Trong mô hình OSI (Open Systems Interconnection), lớp nào chịu trách nhiệm quản lý việc truyền dữ liệu giữa các thiết bị trong mạng?', 1, 1, 5),
-('Từ "Firewall" trong ngữ cảnh mạng máy tính ám chỉ gì?', 1, 1, 5),
-('Đâu là giao thức truyền dữ liệu nhanh và an toàn được sử dụng rộng rãi trong việc tải tệp và trang web?', 1, 1, 5),
-('Trong mạng Wi-Fi, SSID là gì?', 1, 1, 5),
-('Cổng (port) mạng máy tính là gì?', 1, 1, 5),
+('Trong mô hình OSI (Open Systems Interconnection), lớp nào chịu trách nhiệm quản lý việc truyền dữ liệu giữa các thiết bị trong mạng?', 2, 1, 5),
+('Từ "Firewall" trong ngữ cảnh mạng máy tính ám chỉ gì?', 2, 1, 5),
+('Đâu là giao thức truyền dữ liệu nhanh và an toàn được sử dụng rộng rãi trong việc tải tệp và trang web?', 2, 1, 5),
+('Trong mạng Wi-Fi, SSID là gì?', 2, 1, 5),
+('Cổng (port) mạng máy tính là gì?', 2, 1, 5),
 
 --Mức khó (5) câu
 
 
-('Trong mô hình OSI, lớp nào có trách nhiệm kiểm soát việc truyền dữ liệu giữa hai máy tính trực tiếp kết nối với nhau?', 1, 1, 5),
-('Từ "VLAN" là viết tắt của gì trong mạng máy tính?', 1, 1, 5),
-('Trong giao thức mạng, POP và IMAP liên quan đến việc gì?', 1, 1, 5),
-('Trong mạng máy tính, khái niệm "NAT" (Network Address Translation) liên quan đến việc gì?', 1, 1, 5),
-('Trong mạng máy tính, để truy cập một trang web bằng tên miền, máy tính của bạn cần phải thực hiện quá trình nào để tìm địa chỉ IP tương ứng của tên miền đó?', 1, 1, 5),
+('Trong mô hình OSI, lớp nào có trách nhiệm kiểm soát việc truyền dữ liệu giữa hai máy tính trực tiếp kết nối với nhau?', 3, 1, 5),
+('Từ "VLAN" là viết tắt của gì trong mạng máy tính?', 3, 1, 5),
+('Trong giao thức mạng, POP và IMAP liên quan đến việc gì?', 3, 1, 5),
+('Trong mạng máy tính, khái niệm "NAT" (Network Address Translation) liên quan đến việc gì?', 3, 1, 5),
+('Trong mạng máy tính, để truy cập một trang web bằng tên miền, máy tính của bạn cần phải thực hiện quá trình nào để tìm địa chỉ IP tương ứng của tên miền đó?', 3, 1, 5),
 
 --An toàn thông tin
 
@@ -87,16 +87,16 @@ VALUES
 
 --Mức trung bình (5) câu
 
-('Công cụ "wireshark" được sử dụng trong mục đích gì trong lĩnh vực an toàn thông tin?', 1, 1, 6),
-('Phương pháp "shoulder surfing" trong an toàn thông tin liên quan đến việc gì?', 1, 1, 6),
-('Đâu là ví dụ về phần mềm bảo mật được sử dụng để bảo vệ máy tính khỏi các mối đe dọa trực tuyến?', 1, 1, 6),
-('Mã PIN (Personal Identification Number) thường được sử dụng trong ngữ cảnh nào để xác thực người dùng?', 1, 1, 6),
-('Phần mềm "phishing" thường được sử dụng để làm gì?', 1, 1, 6),
+('Công cụ "wireshark" được sử dụng trong mục đích gì trong lĩnh vực an toàn thông tin?', 2, 1, 6),
+('Phương pháp "shoulder surfing" trong an toàn thông tin liên quan đến việc gì?', 2, 1, 6),
+('Đâu là ví dụ về phần mềm bảo mật được sử dụng để bảo vệ máy tính khỏi các mối đe dọa trực tuyến?', 2, 1, 6),
+('Mã PIN (Personal Identification Number) thường được sử dụng trong ngữ cảnh nào để xác thực người dùng?', 2, 1, 6),
+('Phần mềm "phishing" thường được sử dụng để làm gì?', 2, 1, 6),
 
 --Mức khó (5) câu
 
-('Khái niệm "Zero-Day Exploit" liên quan đến việc gì trong lĩnh vực an toàn thông tin?', 1, 1, 6),
-('Trong lĩnh vực an toàn thông tin, khái niệm "Phreaking" liên quan đến việc gì?', 1, 1, 6),
-('Hình thức tấn công "Man-in-the-Middle (MITM)" trong an toàn thông tin liên quan đến việc gì?', 1, 1, 6),
-('Phương pháp "Social Engineering" trong an toàn thông tin liên quan đến việc gì?', 1, 1, 6),
-('Trong bảo mật thông tin, một "honeypot" là gì?', 1, 1, 6);
+('Khái niệm "Zero-Day Exploit" liên quan đến việc gì trong lĩnh vực an toàn thông tin?', 3, 1, 6),
+('Trong lĩnh vực an toàn thông tin, khái niệm "Phreaking" liên quan đến việc gì?', 3, 1, 6),
+('Hình thức tấn công "Man-in-the-Middle (MITM)" trong an toàn thông tin liên quan đến việc gì?', 3, 1, 6),
+('Phương pháp "Social Engineering" trong an toàn thông tin liên quan đến việc gì?', 3, 1, 6),
+('Trong bảo mật thông tin, một "honeypot" là gì?', 3, 1, 6);
