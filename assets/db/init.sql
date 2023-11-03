a568e7d46bd250189ab913fd63cf4c3d973af455
quandohong28 <quandohong28@gmail.com>
2023-11-03 21:12:23 +0700

    update some thing

--- assets/db/init.sql
+++ assets/db/init.sql
@@ -1,17 +1,23 @@
+
 -- MULTIPLE CHOICE TEST
+
 -- auth: quandohong28, chunghieu, vanthanh
 -- version: 0.0.1 beta test
 -- create at: 30/10/2023
+
 -- đang tạo cơ sở dữ liệu multiple_choice_test
 CREATE DATABASE multiple_choice_test;
+
 -- sử dụng multiple_choice_test
 USE multiple_choice_test;
+
 -- đang tạo bảng phân quyền tài khoản
 CREATE TABLE roles (
     id INT(11) NOT NULL DEFAULT 1,
     role VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
+
 -- đang tạo bảng tài khoản
 CREATE TABLE accounts (
     id INT(11) NOT NULL AUTO_INCREMENT,
@@ -27,6 +33,7 @@ CREATE TABLE accounts (
     PRIMARY KEY (id, username),
     FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE
 );
+
 -- đang tạo bảng chuyên mục
 CREATE TABLE categories (
     id INT(11) NOT NULL AUTO_INCREMENT,
@@ -34,18 +41,21 @@ CREATE TABLE categories (
     image VARCHAR(255),
     PRIMARY KEY (id)
 );
+
 -- đang tạo bảng độ khó của câu hỏi
 CREATE TABLE question_levels (
     id INT(11) NOT NULL AUTO_INCREMENT,
     level VARCHAR(50) NOT NULL,
     PRIMARY KEY (id)
 );
+
 -- đang tạo bảng loại (sử dụng cho bảng câu hỏi và bảng bài thi)
 CREATE TABLE types (
     id INT(11) NOT NULL AUTO_INCREMENT,
     type VARCHAR(50) NOT NULL,
     PRIMARY KEY (id)
 );
+
 -- đang tạo bảng câu hỏi
 CREATE TABLE questions (
     id INT(11) NOT NULL AUTO_INCREMENT,
@@ -59,6 +69,7 @@ CREATE TABLE questions (
     FOREIGN KEY (question_level_id) REFERENCES question_levels (id) ON DELETE CASCADE,
     FOREIGN KEY (question_type_id) REFERENCES types (id) ON DELETE CASCADE
 );
+
 -- đang tạo bảng câu trả lời
 CREATE TABLE answers (
     id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -68,6 +79,7 @@ CREATE TABLE answers (
     question_id INT(11) NOT NULL,
     FOREIGN KEY (question_id) REFERENCES questions (id) ON DELETE CASCADE
 );
+
 -- đang tạo bảng danh sách lịch thi
 CREATE TABLE schedules (
     id INT(11) NOT NULL AUTO_INCREMENT,
@@ -79,6 +91,7 @@ CREATE TABLE schedules (
     number_account INT(11) NOT NULL,
     PRIMARY KEY (id)
 );
+
 -- đang tạo bảng các thí sinh có mặt trong 1 lịch thi
 CREATE TABLE candidates (
     id INT(11) NOT NULL AUTO_INCREMENT,
@@ -89,6 +102,7 @@ CREATE TABLE candidates (
     FOREIGN KEY (test_schedule_id) REFERENCES schedules (id) ON DELETE CASCADE,
     FOREIGN KEY (account_id) REFERENCES accounts (id) ON DELETE CASCADE
 );
+
 -- đang tạo bảng danh sách các bài thi
 CREATE TABLE list_exams (
     id INT(11) NOT NULL AUTO_INCREMENT,
@@ -102,6 +116,7 @@ CREATE TABLE list_exams (
     FOREIGN KEY (exam_type_id) REFERENCES types (id) ON DELETE CASCADE,
     FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE
 );
+
 -- đang tạo bảng 1 bài thi chi tiết
 CREATE TABLE exam_details (
     id INT(11) NOT NULL AUTO_INCREMENT,
@@ -111,9 +126,11 @@ CREATE TABLE exam_details (
     FOREIGN KEY (exam_id) REFERENCES list_exams (id) ON DELETE CASCADE,
     FOREIGN KEY (question_id) REFERENCES questions (id) ON DELETE CASCADE
 );
+
 -- đang liên kết bảng thí sinh với bảng chi tiết đề thi
 ALTER TABLE candidates
 ADD FOREIGN KEY (exam_detail_id) REFERENCES exam_details (id);
+
 -- đang tạo bảng danh sách những bài thi đang làm
 create table doing_exams (
     id INT(11) NOT NULL AUTO_INCREMENT,
@@ -123,6 +140,7 @@ create table doing_exams (
     foreign key (account_id) references accounts(id) ON DELETE CASCADE,
     foreign key (exam_detail_id) references exam_details(id) ON DELETE CASCADE
 );
+
 -- đang tạo bảng những câu hỏi đã chọn - trong khi thi - có thể sửa dữ liệu
 create table completed_questions (
     id INT(11) NOT NULL AUTO_INCREMENT,
@@ -133,6 +151,7 @@ create table completed_questions (
     foreign key (question_id) references questions(id) ON DELETE CASCADE,
     foreign key (doing_exam_id) references doing_exams(id) ON DELETE CASCADE
 );
+
 -- đang tạo bảng những câu hỏi đã chọn - đã thi - không thể sửa dữ liệu
 create table completed_exams (
     id INT(11) NOT NULL AUTO_INCREMENT,
@@ -143,6 +162,7 @@ create table completed_exams (
     foreign key (question_id) references questions(id) ON DELETE CASCADE,
     FOREIGN KEY (exam_detail_id) REFERENCES exam_details(id) ON DELETE CASCADE
 );
+
 -- đang tạo bảng danh sách kết quả của những bài thi đã thi
 CREATE TABLE results (
     id INT(11) NOT NULL AUTO_INCREMENT,
@@ -151,6 +171,7 @@ CREATE TABLE results (
     account_id INT(11) NOT NULL,
     FOREIGN KEY (exam_id) REFERENCES list_exams (id) ON DELETE CASCADE
 );
+
 -- đang tạo bảng kết quả của 1 bài thi chi tiết
 CREATE TABLE result_details (
     id INT(11) NOT NULL AUTO_INCREMENT,
@@ -162,15 +183,24 @@ CREATE TABLE result_details (
     FOREIGN KEY (account_id) REFERENCES accounts (id) ON DELETE CASCADE,
     FOREIGN KEY (completed_exam_id) REFERENCES completed_exams (id) ON DELETE CASCADE
 );
--- đang nhập dữ liệu cho bảng loại câu hỏi
+
+-- đang nhập dữ liệu cho bảng độ khó câu hỏi
 INSERT INTO question_levels (id, level)
 VALUES (1, 'easy'),
     VALUES (2, 'medium'),
     VALUES (3, 'hard');
+
 -- đang nhập dữ liệu cho bảng quyền tài khoản
 INSERT INTO roles (id, role)
 VALUES (0, 'admin'),
     VALUES (1, 'user');
+
+-- đang nhập dữ liệu cho bảng loại câu hỏi
+INSERT INTO types (type)
+VALUES
+    ('practice_test'),
+    ('real_test');
+
 -- đang nhập dữ liệu cho bảng chuyên mục
 INSERT INTO categories('name', 'image')
 VALUES ('Công nghệ phần mềm', ''),
