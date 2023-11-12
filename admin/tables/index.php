d5304ac9716515cd5d78c794f7da7e5e9c9fd8b4
hieukhocnhe <trantrunghieu422@gmail.com>
2023-11-12 10:06:21 +0700

    complete function delete schedule

--- admin/tables/index.php
+++ admin/tables/index.php
@@ -2,24 +2,35 @@
 	<!-- Tabs navs -->
 	<ul class="nav nav-tabs m-3" id="ex-with-icons" role="tablist">
 		<li class="nav-item" role="presentation">
-			<a class="nav-link" id="ex-with-icons-tab-1" data-mdb-toggle="tab" href="?act=tables&data=accounts" role="tab" aria-controls="ex-with-icons-tabs-1" aria-selected="true"><i class="fas fa-solid fa-users me-2"></i>Tài khoản</a>
+			<a class="nav-link" id="ex-with-icons-tab-1" data-mdb-toggle="tab" href="?act=tables&data=accounts"
+				role="tab" aria-controls="ex-with-icons-tabs-1" aria-selected="true"><i
+					class="fas fa-solid fa-users me-2"></i>Tài khoản</a>
 		</li>
 		<li class="nav-item" role="presentation">
-			<a class="nav-link" id="ex-with-icons-tab-2" data-mdb-toggle="tab" href="?act=tables&data=categories" role="tab" aria-controls="ex-with-icons-tabs-2" aria-selected="false"><i class="fas fa-chart-line fa-list me-2"></i>Chuyên mục</a>
+			<a class="nav-link" id="ex-with-icons-tab-2" data-mdb-toggle="tab" href="?act=tables&data=categories"
+				role="tab" aria-controls="ex-with-icons-tabs-2" aria-selected="false"><i
+					class="fas fa-chart-line fa-list me-2"></i>Chuyên mục</a>
 		</li>
 		<li class="nav-item" role="presentation">
-			<a class="nav-link" id="ex-with-icons-tab-3" data-mdb-toggle="tab" href="?act=tables&data=schedules" role="tab" aria-controls="ex-with-icons-tabs-3" aria-selected="false"><i class="fas fa-calendar-days fa-fw me-2"></i>Lịch thi</a>
+			<a class="nav-link" id="ex-with-icons-tab-3" data-mdb-toggle="tab" href="?act=tables&data=schedules"
+				role="tab" aria-controls="ex-with-icons-tabs-3" aria-selected="false"><i
+					class="fas fa-calendar-days fa-fw me-2"></i>Lịch thi</a>
 		</li>
 		<li class="nav-item" role="presentation">
-			<a class="nav-link" id="ex-with-icons-tab-3" data-mdb-toggle="tab" href="?act=tables&data=questions" role="tab" aria-controls="ex-with-icons-tabs-3" aria-selected="false"><i class="fas fa-question fa-fw me-2"></i>Câu
+			<a class="nav-link" id="ex-with-icons-tab-3" data-mdb-toggle="tab" href="?act=tables&data=questions"
+				role="tab" aria-controls="ex-with-icons-tabs-3" aria-selected="false"><i
+					class="fas fa-question fa-fw me-2"></i>Câu
 				hỏi</a>
 		</li>
 		<li class="nav-item" role="presentation">
-			<a class="nav-link" id="ex-with-icons-tab-3" data-mdb-toggle="tab" href="?act=tables&data=results" role="tab" aria-controls="ex-with-icons-tabs-3" aria-selected="false"><i class="fa-solid fa-square-poll-vertical me-2"></i>Theo
+			<a class="nav-link" id="ex-with-icons-tab-3" data-mdb-toggle="tab" href="?act=tables&data=results"
+				role="tab" aria-controls="ex-with-icons-tabs-3" aria-selected="false"><i
+					class="fa-solid fa-square-poll-vertical me-2"></i>Theo
 				dõi điểm</a>
 		</li>
 		<li class="nav-item" role="presentation">
-			<a class="nav-link" id="ex-with-icons-tab-3" data-mdb-toggle="tab" href="?act=tables&data=exams" role="tab" aria-controls="ex-with-icons-tabs-3" aria-selected="false"><i class="fa-solid fa-file-lines me-2"></i>Đề
+			<a class="nav-link" id="ex-with-icons-tab-3" data-mdb-toggle="tab" href="?act=tables&data=exams" role="tab"
+				aria-controls="ex-with-icons-tabs-3" aria-selected="false"><i class="fa-solid fa-file-lines me-2"></i>Đề
 				thi</a>
 		</li>
 	</ul>
