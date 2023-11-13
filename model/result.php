ff48235cb178bbdcb2214bb98dc0dcad91946761
hieukhocnhe <trantrunghieu422@gmail.com>
2023-11-13 20:27:03 +0700

    changed some code for result

--- model/result.php
+++ model/result.php
@@ -2,8 +2,13 @@
 function getAllResults()
 {
     try {
-        $sql = "SELECT * FROM results";
-        return pdo_execute($sql);
+        $sql = "SELECT 
+        r.id AS id, e.id AS exam_code, e.exam_type_id AS exam_type, a.fullname AS fullname,
+        r.points AS points, r.time_start AS time_start, r.exam_time AS exam_time FROM results r 
+        INNER JOIN accounts a ON a.id = r.account_id 
+        INNER JOIN exams e ON e.id = r.exam_id
+        INNER JOIN types t ON t.id = e.exam_type_id;";
+        return pdo_query($sql);
     } catch (Exception $e) {
         return $e->getMessage();
     }
@@ -40,10 +45,15 @@ function getResultsByExamId($exam_id)
     }
 }
 
-function getResultDetails($result_id)
+function getResultDetails($id)
 {
     try {
-        $sql = "SELECT * FROM result_details WHERE result_id = $result_id";
+        $sql = "SELECT 
+        rd.id AS result_detail_id, q.content AS question_content, 
+        a.content as answer_content FROM result_details rd 
+        INNER JOIN questions q ON q.id = rd.id
+        INNER JOIN answers a ON a.id = rd.id
+        WHERE rd.id = '$id';";
         return pdo_query($sql);
     } catch (Exception $e) {
         return $e->getMessage();
