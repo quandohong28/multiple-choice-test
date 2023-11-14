23a9b348b8bb70878afa94d093bed1d603f1c8ff
quandohong28 <quandohong28@gmail.com>
2023-11-14 14:38:55 +0700

    okela

--- admin/index.php
+++ admin/index.php
@@ -19,7 +19,6 @@ $action = isset($_GET['act']) ? $_GET['act'] : 'dashboard';
     <!-- Bootstrap CSS v5.2.1 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link rel="stylesheet" href="../assets/css/styles.admin.css">
-    <!-- CSS thuần -->
     <!-- Goole font -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <!-- font-awesome -->
--- admin/tables/components/data_export.php
+++ admin/tables/components/data_export.php
@@ -1,20 +1,20 @@
 <div class="row">
     <div class="btn-group float-end">
         <button class="btn btn-sm btn-success rounded-1 py-1 px-2 me-2">
-            <i class="fa-solid fa-file-export me-2"></i>
-            <span>Excel</span>
+            <i class="fa-solid fw-fs fa-file-export"></i>
+            <span class="md-hidden">Excel</span>
         </button>
         <button class="btn btn-sm btn-outline-success rounded-1 py-1 px-2 me-2" disabled>
-            <i class="fa-solid fa-print me-2"></i>
-            <span>Print</span>
+            <i class="fa-solid fw-fs fa-print"></i>
+            <span class="md-hidden">Print</span>
         </button>
         <button class="btn btn-sm btn-outline-success rounded-1 py-1 px-2 me-2" disabled>
-            <i class="fa-solid fa-file-csv me-2"></i>
-            <span>CSV</span>
+            <i class="fa-solid fw-fs fa-file-csv"></i>
+            <span class="md-hidden">CSV</span>
         </button>
         <button class="btn btn-sm btn-outline-success rounded-1 py-1 px-2" disabled>
-            <i class="fa-solid fa-file-pdf me-2"></i>
-            <span>PDF</span>
+            <i class="fa-solid fw-fs fa-file-pdf"></i>
+            <span class="md-hidden">PDF</span>
         </button>
     </div>
 </div>
