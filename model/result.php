8d5ca397b0ad82f1d73d7ee0f4c4a30bc90ce792
Tnenl <anhdo13072004@gmail.com>
2023-11-25 22:56:50 +0700

    ogela

--- model/result.php
+++ model/result.php
@@ -175,7 +175,8 @@ function reloadStatusResult($id)
         r.id as result_id,
         r.time_start as time_start,
         e.exam_time as exam_time,
-        r.status as result_status
+        r.status as result_status,
+        r.exam_id as exam_id
     FROM
         results r
     INNER JOIN exams e ON
@@ -191,6 +192,7 @@ function reloadStatusResult($id)
         $results = pdo_query($sql1);
         foreach ($results as $result) {
             extract($result);
+            $exam_id = $exam_id;
             $result_id = $result_id;
             $result_status = $result_status;
             $time_start = $time_start;
@@ -198,14 +200,47 @@ function reloadStatusResult($id)
             $time_end = date('Y-m-d H:i:s', strtotime($time_start) + ($exam_time * 60));
             $dt = new DateTime('now', new DateTimeZone('Asia/Ho_Chi_Minh'));
             $current_time = $dt->format('Y-m-d H:i:s');
-            $exam_time_finish = sprintf('%02d:%02d:%02d', ($exam_time / 60), ($exam_time % 60),0);
-            if ($result_status == 0) { 
+            $exam_time_finish = sprintf('%02d:%02d:%02d', ($exam_time / 60), ($exam_time % 60), 0);
+            if ($result_status == 0) {
                 if ($current_time >= $time_end) {
                     $sql = "UPDATE results SET status = 1, exam_time = '$exam_time_finish' WHERE id = $result_id";
+                    examResult($exam_id);
                     pdo_execute($sql);
                 }
             }
         }
+    } catch (\Exception $e) {
+        echo $e->getMessage();
+    }
+}
+
+function examResult($exam_id)
+{
+    try {
+        $sql = "SELECT 
+        rd.result_id,
+        rd.question_id,
+        rd.answer_id,
+        a.id AS answer_correct
+    FROM result_details rd
+    JOIN questions q ON q.id = rd.question_id
+    JOIN results r ON r.id = rd.result_id
+    JOIN answers a ON a.question_id = q.id 
+    WHERE r.exam_id = '$exam_id' AND a.is_correct = 1;";
+
+        $result_details = pdo_query($sql);
+
+        $points = 0;
+        $maxPoints = 10 / count($result_details);
+        foreach ($result_details as $result_detail) {
+            extract($result_detail);
+            if ($answer_id == $answer_correct) {
+                $points += $maxPoints;
+            }
+        }
+        $points = round($points, 1);
+        $sql = "UPDATE results SET points = '$points' WHERE id = '$result_id'";
+        pdo_execute($sql);
     } catch (\Exception $e) {
         echo $e->getMessage();
     }
